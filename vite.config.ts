56b69f66fe112f2643725d9585c7dd2feb360412
Matthieu Borgognon <mat.gh@bqn.ch>
2025-11-17 19:31:15 +0100

    ci: fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
   return {
-    base: env.BASE_URL || '/',
+    base: process.env.BASE_URL || '/p3fo/',
     plugins: [dyadComponentTagger(), react()],
     server: {
       host: true, // This makes the dev server listen on all network interfaces
